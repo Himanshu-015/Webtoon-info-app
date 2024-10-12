b6e33ff23ff3f75a4d0038246c576df6d510784e
Himanshu-015 <himanshutamer015@gmail.com>
2024-10-12 14:50:06 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,9 @@
+/*  PLEASE CONFIGURE THOSE VALUES OR OKUMA WON'T WORK.
+    booksURL can be on a different domain/subdomain if you prefer.
+    The books are stored in a library. The library name can be whatever you want (here it's books)
+*/
+export function homeURL() {return           "https://drmint.github.io/Okuma-Reader/"}
+export function readerURL() {return         "https://drmint.github.io/Okuma-Reader/read.html"}
+export function booksURL() {return          "https://drmint.github.io/Okuma-Library/books/"}
+export function websiteName() {return       "Okuma Example"}
+export function defaultLanguage() {return   "en"}
